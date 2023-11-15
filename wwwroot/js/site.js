92650470f109f411c168ea4f4d37318ac7b818ca
adanSmith01 <smith.paredes.13@gmail.com>
2023-11-15 12:24:40 -0300

    Se agrego una funcion para el color de la tarea

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function getColor(){
+    var color = document.getElementById("colorTarea");
+    var string_color = document.getElementById("Color");
 
-// Write your JavaScript code.
+    string_color.value = color.value;
+}
