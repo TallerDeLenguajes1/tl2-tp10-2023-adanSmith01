13cba4f7a056492237bb535001f9c5a3d8df4551
adanSmith01 <smith.paredes.13@gmail.com>
2023-11-18 21:49:48 -0300

    Se cambio el modelo de usuario

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,10 +1,39 @@
+using tl2_tp10_2023_adanSmith01.ViewModels;
+
 namespace tl2_tp10_2023_adanSmith01.Models;
+
+public enum Rol
+{
+    Administrador,
+    Operador
+}
 
 public class Usuario
 {
     private int id;
     private string nombreUsuario;
-
+    private string contrasenia;
+    private Rol rolUsuario;
     public int Id { get => id; set => id = value; }
     public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+    public string Contrasenia { get => contrasenia; set => contrasenia = value; }
+    public Rol RolUsuario { get => rolUsuario; set => rolUsuario = value; }
+
+    public Usuario(CrearUsuarioViewModel usuario){
+        this.nombreUsuario = usuario.Nombre;
+        this.contrasenia = usuario.Contrasenia;
+        this.rolUsuario = usuario.RolUsuario;
+    }
+
+    public Usuario(ActualizarUsuarioViewModel usuario){
+        this.id = usuario.IdUsuario;
+        this.nombreUsuario = usuario.Nombre;
+        this.contrasenia = usuario.Contrasenia;
+        this.rolUsuario = usuario.RolUsuario;
+    }
+
+
+    public Usuario(){
+        
+    }
 }
