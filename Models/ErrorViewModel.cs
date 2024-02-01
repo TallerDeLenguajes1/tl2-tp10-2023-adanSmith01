20471e10fbfd156a14d3d1b596ace80d282cd3b4
adanSmith01 <smith.paredes.13@gmail.com>
2024-01-31 22:34:42 -0300

    Se movio el archivo.cs al directorio de ViewModels

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace tl2_tp10_2023_adanSmith01.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
