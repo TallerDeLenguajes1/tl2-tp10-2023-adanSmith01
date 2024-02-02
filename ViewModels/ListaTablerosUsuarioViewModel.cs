2a74a7a88bb2a464336a669d17b2764e232b6575
adanSmith01 <smith.paredes.13@gmail.com>
2024-02-02 01:41:20 -0300

    ViewModel para mostrar los tableros de todos los usuarios o de un usuario especifico

--- ViewModels/ListaTablerosUsuarioViewModel.cs
+++ ViewModels/ListaTablerosUsuarioViewModel.cs
@@ -3,54 +3,21 @@ using tl2_tp10_2023_adanSmith01.Models;
 
 public class ListaTablerosUsuarioViewModel
 {
-    private bool habilitarEnlace;
-    private bool hacerBMTablero;
-    private List<TableroViewModel> tablerosPropios;
-    private List<TableroViewModel> tablerosConYSinTareasAsignadas;
+    private List<TableroViewModel> tablerosUsuario;
     private List<UsuarioViewModel> usuariosPropietarios;
-    private UsuarioViewModel usuarioPropietario;
 
-    public List<TableroViewModel> TablerosPropios { get => tablerosPropios;}
-    public List<UsuarioViewModel> UsuariosPropietarios { get => usuariosPropietarios;}
-    public bool HabilitarEnlace { get => habilitarEnlace;}
-    public bool HacerBMTablero { get => hacerBMTablero;}
-    public List<TableroViewModel> TablerosConYSinTareasAsignadas { get => tablerosConYSinTareasAsignadas; set => tablerosConYSinTareasAsignadas = value; }
-    public UsuarioViewModel UsuarioPropietario { get => usuarioPropietario; set => usuarioPropietario = value; }
-
-    public ListaTablerosUsuarioViewModel(List<Tablero> tablerosPropios, List<Usuario> usuariosPropietarios)
+    public ListaTablerosUsuarioViewModel(List<Tablero> tablerosUsuario, List<Usuario> usuariosPropietarios)
     {
-        this.tablerosPropios = new List<TableroViewModel>();
+        this.tablerosUsuario = new List<TableroViewModel>();
         this.usuariosPropietarios = new List<UsuarioViewModel>();
-        this.tablerosConYSinTareasAsignadas = new List<TableroViewModel>();
-        foreach(var tablero in tablerosPropios) this.tablerosPropios.Add(new TableroViewModel(tablero));
+        foreach(var tablero in tablerosUsuario)
+        {
+            if(string.IsNullOrEmpty(tablero.Descripcion)) tablero.Descripcion = "Sin Descripcion";
+            this.tablerosUsuario.Add(new TableroViewModel(tablero));
+        }
         foreach(var usuario in usuariosPropietarios) this.usuariosPropietarios.Add(new UsuarioViewModel(usuario));
-        habilitarEnlace = false;
-        hacerBMTablero = true;
     }
 
-    public ListaTablerosUsuarioViewModel(List<Tablero> tablerosPropios, Usuario usuarioPropietario)
-    {
-        this.tablerosPropios = new List<TableroViewModel>();
-        this.usuariosPropietarios = new List<UsuarioViewModel>();
-        this.usuarioPropietario = new UsuarioViewModel(usuarioPropietario);
-        this.tablerosConYSinTareasAsignadas = new List<TableroViewModel>();
-        foreach(var tablero in tablerosPropios) this.tablerosPropios.Add(new TableroViewModel(tablero));
-        habilitarEnlace = false;
-        hacerBMTablero = true;
-    }
-
-    public ListaTablerosUsuarioViewModel(List<Tablero> tablerosPropios, Usuario usuarioPropietario, List<Tablero> tablerosConYSinTareasAsignadas, List<Usuario> usuariosPropietarios)
-    {
-        this.tablerosPropios = new List<TableroViewModel>();
-        this.tablerosConYSinTareasAsignadas = new List<TableroViewModel>();
-        this.usuariosPropietarios = new List<UsuarioViewModel>();
-        this.usuarioPropietario = new UsuarioViewModel(usuarioPropietario);
-        foreach(var tablero in tablerosPropios) this.tablerosPropios.Add(new TableroViewModel(tablero));
-        foreach(var tablero in tablerosConYSinTareasAsignadas) this.tablerosConYSinTareasAsignadas.Add(new TableroViewModel(tablero));
-        foreach(var usuario in usuariosPropietarios) this.usuariosPropietarios.Add(new UsuarioViewModel(usuario));
-        habilitarEnlace = true;
-        hacerBMTablero = true;
-    }
-
-
+    public List<TableroViewModel> TablerosUsuario { get => tablerosUsuario; }
+    public List<UsuarioViewModel> UsuariosPropietarios { get => usuariosPropietarios; }
 }
