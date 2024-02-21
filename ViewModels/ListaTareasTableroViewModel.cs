93ddcb59e04fc9dd5a0c1d74a2dbb4ea34190433
adanSmith01 <smith.paredes.13@gmail.com>
2024-02-21 02:36:25 -0300

    Se modifico la lista de tareas por un diccionario con clave igual al estado de la tarea y la lista de tareas correspondiente por valor

--- ViewModels/ListaTareasTableroViewModel.cs
+++ ViewModels/ListaTareasTableroViewModel.cs
@@ -4,22 +4,30 @@ using tl2_tp10_2023_adanSmith01.Models;
 public class ListaTareasTableroViewModel
 {
     private TableroViewModel tablero;
-    private List<TareaViewModel> tareasTablero;
+    private Dictionary<EstadoTarea, List<TareaViewModel>> tareasTableroPorEstado;
     private List<UsuarioViewModel> usuariosAsignados;
+    private bool habilitarCrearTarea;
 
     public TableroViewModel Tablero { get => tablero; set => tablero = value; }
-    public List<TareaViewModel> TareasTablero { get => tareasTablero; set => tareasTablero = value; }
+    public Dictionary<EstadoTarea, List<TareaViewModel>> TareasTableroPorEstado { get => tareasTableroPorEstado; set => tareasTableroPorEstado = value; }
     public List<UsuarioViewModel> Usuarios { get => usuariosAsignados; set => usuariosAsignados = value; }
+    public bool HabilitarCrearTarea { get => habilitarCrearTarea; }
 
-    public ListaTareasTableroViewModel(Tablero tablero, List<Tarea> tareasTablero, List<Usuario> usuariosAsignados){
+    public ListaTareasTableroViewModel(Tablero tablero, Dictionary<EstadoTarea, List<Tarea>> tareasTableroPorEstado, List<Usuario> usuariosAsignados,bool habilitarCrearTarea){
         this.usuariosAsignados = new List<UsuarioViewModel>();
-        this.tareasTablero = new List<TareaViewModel>();
+        this.tareasTableroPorEstado = new Dictionary<EstadoTarea, List<TareaViewModel>>();
         this.tablero = new TableroViewModel(tablero);
-        foreach(var tarea in tareasTablero) 
+        foreach(var tareasTablero in tareasTableroPorEstado)
         {
-            if(string.IsNullOrEmpty(tarea.Descripcion)) tarea.Descripcion = "Sin descripcion";
-            this.tareasTablero.Add(new TareaViewModel(tarea));
+            var tareas = new List<TareaViewModel>();
+            foreach(var tarea in tareasTablero.Value)
+            {
+                if(string.IsNullOrEmpty(tarea.Descripcion)) tarea.Descripcion = "Sin descripción";
+                tareas.Add(new TareaViewModel(tarea));
+            }
+            this.tareasTableroPorEstado.Add(tareasTablero.Key, tareas);
         }
         foreach(var usuario in usuariosAsignados) this.usuariosAsignados.Add(new UsuarioViewModel(usuario));
+        this.habilitarCrearTarea = habilitarCrearTarea;
     }
 }
