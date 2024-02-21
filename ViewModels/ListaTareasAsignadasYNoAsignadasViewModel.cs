9f94a1ae61b5f0dca07a06697fcc401df6e8838a
adanSmith01 <smith.paredes.13@gmail.com>
2024-02-21 02:35:49 -0300

    Se modifico la lista de tareas por un diccionario con clave igual al estado de la tarea y la lista de tareas correspondiente por valor

--- ViewModels/ListaTareasAsignadasYNoAsignadasViewModel.cs
+++ ViewModels/ListaTareasAsignadasYNoAsignadasViewModel.cs
@@ -4,17 +4,26 @@ using tl2_tp10_2023_adanSmith01.Models;
 public class ListaTareasAsignadasYNoAsignadasViewModel
 {
     private TableroViewModel tableroCTA;
-    private List<TareaViewModel> tareasAsignadasYNoAsignadas;
+    private Dictionary<EstadoTarea, List<TareaViewModel>> tareasAsignadasYNoAsignadas;
     private UsuarioViewModel usuarioAsignado;
 
-    public List<TareaViewModel> TareasAsignadasYNoAsignadas { get => tareasAsignadasYNoAsignadas; }
+    public Dictionary<EstadoTarea, List<TareaViewModel>> TareasAsignadasYNoAsignadas { get => tareasAsignadasYNoAsignadas; }
     public UsuarioViewModel UsuarioAsignado { get => usuarioAsignado; }
     public TableroViewModel TableroCTA { get => tableroCTA; }
 
-    public ListaTareasAsignadasYNoAsignadasViewModel(Tablero tableroCTA, List<Tarea> tareasAsignadasYNoAsignadas, Usuario usuarioAsignado){
-        this.tareasAsignadasYNoAsignadas = new List<TareaViewModel>();
+    public ListaTareasAsignadasYNoAsignadasViewModel(Tablero tableroCTA, Dictionary<EstadoTarea, List<Tarea>> tareasAsignadasYNoAsignadas, Usuario usuarioAsignado){
+        this.tareasAsignadasYNoAsignadas = new Dictionary<EstadoTarea, List<TareaViewModel>>();
         this.usuarioAsignado = new UsuarioViewModel(usuarioAsignado);
         this.tableroCTA = new TableroViewModel(tableroCTA);
-        foreach(var tarea in tareasAsignadasYNoAsignadas) this.tareasAsignadasYNoAsignadas.Add(new TareaViewModel(tarea)); 
+        foreach(var tareasTablero in tareasAsignadasYNoAsignadas)
+        {
+            var tareas = new List<TareaViewModel>();
+            foreach(var tarea in tareasTablero.Value)
+            {
+                if(string.IsNullOrEmpty(tarea.Descripcion)) tarea.Descripcion = "Sin descripción";
+                tareas.Add(new TareaViewModel(tarea));
+            }
+            this.tareasAsignadasYNoAsignadas.Add(tareasTablero.Key, tareas);
+        } 
     }
 }
